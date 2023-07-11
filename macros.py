758fa7fa0e5eb9c7d499c4929ecc13492a9c51e0
Christopher Petrilli <petrilli@amber.org>
2023-07-10 21:20:27 -0700

    First couple live circuits

--- macros.py
+++ macros.py
@@ -3,34 +3,8 @@ from lzstring import LZString
 import os
 
 
-# Example from https://github.com/pfalstad/circuitjs1/blob/master/tests/jfet1.txt
-EXAMPLE_SCHEMATIC = """$ 1 0.000005 7.619785657297057 56 5 50
-v 432 368 432 224 0 1 40 3 0 0 0.5
-r 432 224 592 224 0 10
-w 592 224 672 224 0
-w 672 224 672 272 0
-w 672 304 672 368 0
-r 672 368 544 368 0 100
-g 544 368 544 400 0
-w 432 368 544 368 0
-r 544 288 544 368 0 1
-j 592 288 672 288 0 -3.15 0.00412
-w 544 288 592 288 0
-w 672 368 704 368 0
-w 672 224 704 224 0
-w 544 288 544 256 0
-j 800 256 832 256 0 -4 0.00125
-w 800 256 768 256 0
-w 832 272 832 304 0
-w 832 240 832 208 0
-L 752 256 720 256 0 1 false 5 0
-L 832 208 832 176 0 0 false 5 0
-L 832 304 832 336 0 0 false 5 0
-r 752 256 768 256 0 1000
-o 5 64 0 12291 0.9681278252715998 0.009681278252715997 0 2 5 3
-"""
-
 BASE_URL_FOR_CIRCUITJS = """http://www.falstad.com/circuit/circuitjs.html"""
+
 
 def define_env(env):
     """This is the hook for defining variables, macros and filters
@@ -44,11 +18,11 @@ def define_env(env):
     def compress_to_url(s: str) -> str:
         """Convert an input string to URL-encoded compressed string."""
         output = LZString()
-        
+
         return output.compressToEncodedURIComponent(s)
 
     @env.macro
-    def embed_schematic(schematic_filename: str = None, height: int = 480) -> str:
+    def embed_schematic(schematic_filename: str, height: int = 480) -> str:
         """Generate HTML to embed circuit.js diagram in the document.
 
         Args:
@@ -61,24 +35,24 @@ def define_env(env):
             r"""<iframe width="100%" height="480" src="{url}">OH NOES!</iframe>"""
         )
 
-        if schematic_filename:
-            full_filename = os.path.join(
-                env.project_dir,
-                "circuits",
-                f"{schematic_filename}.circuit")
-            with open(full_filename, "r") as f:
-                schematic = f.read()
-        else:
-            schematic = EXAMPLE_SCHEMATIC
+        full_filename = os.path.join(
+            env.project_dir, "circuits", f"{schematic_filename}.circuit"
+        )
+        with open(full_filename, "r") as f:
+            schematic = f.read()
 
         # These options are derived from circuitjs documentation.
         # https://github.com/pfalstad/circuitjs1#embedding
-        EMBED_OPTIONS = {}
+        EMBED_OPTIONS = {
+            "hideMenu": "true",
+        }
 
         # This will already be base64 URL-safe encoded
         compressed_schematic = compress_to_url(schematic)
 
-        query_string = url.urlencode(EMBED_OPTIONS | dict(ctz=compressed_schematic))
+        query_string = url.urlencode(
+            EMBED_OPTIONS | dict(ctz=compressed_schematic), doseq=True
+        )
 
         final_url = BASE_URL_FOR_CIRCUITJS + "?" + query_string
         print(final_url)
